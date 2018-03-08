327cae6e0a0fa0fc9573457f2f8582ff7a15012f
hontsev <hontsev@gmail.com>
2018-03-08 15:28:00 +0800

    first submit

---
+++ ShiftJisDecoder/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace ShiftJisDecoder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(232, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "用法：把乱码文件拖入此窗口即可";
+            // 
+            // textBox1
+            // 
+            this.textBox1.AcceptsReturn = true;
+            this.textBox1.AcceptsTab = true;
+            this.textBox1.Location = new System.Drawing.Point(12, 164);
+            this.textBox1.MaxLength = 9999999;
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(116, 164);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(162, 164);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox2.Size = new System.Drawing.Size(116, 164);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(134, 230);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = ">>";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(12, 70);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(104, 19);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "处理文件夹";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 38);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(247, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "解码后的文件会被放在相同路径下。";
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Checked = true;
+            this.checkBox2.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox2.Location = new System.Drawing.Point(12, 95);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.checkBox2.Size = new System.Drawing.Size(245, 19);
+            this.checkBox2.TabIndex = 6;
+            this.checkBox2.Text = "处理文本内容（txt,ini,html）";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 146);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(187, 15);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "下面可以粘贴乱码转成日文";
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(290, 340);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "ShiftJisDecoder日语乱码编码转换";
+            this.TopMost = true;
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ ShiftJisDecoder/Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ShiftJisDecoder
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        public static double isJapanese(string str)
+        {
+            const string jcharacter = "ぁあぃいぅうぇえぉおかがきぎくぐけげこごさざしじすずせぜそぞただちぢっつづてでとどなにぬねのはばひびぴふぶぷへべぺほぼぽまみむめもゃやゅゆょよらりるれろゎわゐゑをん゛゜ゝゞァアィイゥウェエォオカガキギクグケゲコゴサザシジスズセゼソゾタダチヂッツヅテデトドナニヌネノハバパヒビピフブプヘベペホボポマミムメモャヤュユョヨラリルレロヮワヰヱヲンヴヵヶーヽヾぁあぃいぅうぇえぉおかがきぎくぐけげこごさざしじすずせぜそぞただちぢっつづてでとどなにぬねのはばひびぴふぶぷへべぺほぼぽまみむめもゃやゅゆょよらりるれろゎわゐゑをんﾞﾟゝゞｧｱｨｲｩｳｪｴｫｵｶﾞｷｸｹｺｻｼｽｾｿﾀﾁｯﾂﾃﾄﾅﾆﾇﾈﾉﾊﾋﾌﾟﾍﾎﾏﾐﾑﾒﾓｬﾔｭﾕｮﾖﾗﾘﾙﾚﾛﾜｲｴｦﾝ";
+            double sum = 0;
+            foreach (var c in str)
+            {
+                if (jcharacter.Contains(c)) sum++;
+            }
+            return sum / str.Length;
+        }
+
+        /// <summary>
+        /// 将错误编码的日文乱码转化为正确的日文
+        /// </summary>
+        /// <param name="errcode"></param>
+        /// <param name="errEncoding"></param>
+        /// <returns></returns>
+        private string convertJEoncode(string errcode, Encoding errEncoding = null)
+        {
+            if (isJapanese(errcode)>=0.001)
+            {
+                //已经被转成日文了，不用转了
+                return errcode;
+            }
+
+            if (errEncoding == null) errEncoding = Encoding.GetEncoding("gb2312");
+
+            var hopefullyRecovered = errEncoding.GetBytes(errcode);
+            var oughtToBeJapanese = Encoding.GetEncoding(932).GetString(hopefullyRecovered);
+
+            return oughtToBeJapanese;
+        }
+
+        private static string removeInvalidChar(string ori)
+        {
+            var invalid = new string(Path.GetInvalidFileNameChars()) + new string(Path.GetInvalidPathChars());
+            foreach (char c in invalid) ori = ori.Replace(c.ToString(), "_");
+            return ori;
+        }
+
+        private string getEnableFilename(string path)
+        {
+            if (!File.Exists(path) && !Directory.Exists(path)) return path;
+            int rename = 1;
+            string dict = Path.GetDirectoryName(path);
+            string name = Path.GetFileNameWithoutExtension(path);
+            string ext = Path.GetExtension(path);
+            while (File.Exists(path) || Directory.Exists(path))
+            {
+                path = string.Format("{0}/{1}({2}){3}", dict, name, rename++, ext);
+            }
+            return path;
+        }
+
+        private void dealOneFile(string path,string dict)
+        {
+            bool deal_text = checkBox2.Checked;
+
+            //print("load:" + path);
+
+            string filename = Path.GetFileNameWithoutExtension(path);
+            string ext = Path.GetExtension(path);
+            // string dict = Path.GetDirectoryName(path);
+
+            string out_filename = removeInvalidChar(convertJEoncode(filename));
+            string out_path = dict + "/" + out_filename + ext;
+            out_path = getEnableFilename(out_path);
+
+            if (deal_text && ".txt .ini .html .js".Contains(ext))
+            {
+                // decoding content text
+
+                string out_filecontent = "";
+                string tmp1 = File.ReadAllText(path, Encoding.GetEncoding(932));
+                string tmp2 = File.ReadAllText(path, Encoding.GetEncoding("gb2312"));
+                string tmp3 = File.ReadAllText(path, Encoding.UTF8);
+                double conf1 = isJapanese(tmp1);
+                double conf2 = isJapanese(tmp2);
+                double conf3 = isJapanese(tmp3);
+                if (conf1>conf2 && conf1>conf3) out_filecontent = tmp1;
+                else if (conf2 > conf1 && conf2 > conf3) out_filecontent = tmp2;
+                else if (conf3 > conf2 && conf3 > conf1) out_filecontent = tmp3;
+
+                if (out_filecontent.Length <= 0) out_filecontent = tmp2;
+
+                File.WriteAllText(out_path, out_filecontent);
+            }
+            else
+            {
+                //copy
+                File.Copy(path, out_path);
+            }
+        }
+
+        private void dealFiles(string[] files,string dict)
+        {
+            bool deal_dir = checkBox1.Checked;
+            
+
+            foreach (var path in files)
+            {
+                if (string.IsNullOrEmpty(dict)) dict = Path.GetDirectoryName(path);
+
+                if(Directory.Exists(path) && deal_dir)
+                {
+                    // deal dir
+                    string dict_name = Path.GetFileName(path);
+                    dict_name= removeInvalidChar(convertJEoncode(dict_name));
+                    string out_dict_name = getEnableFilename(dict + "/" + dict_name)+"/";
+                    Directory.CreateDirectory(out_dict_name);
+                    List<string> all_next_files=new List<string>();
+                    string[] next_dicts = Directory.GetDirectories(path);
+                    string[] next_files = Directory.GetFiles(path);
+                    foreach (var f in next_dicts) all_next_files.Add(f);
+                    foreach (var f in next_files) all_next_files.Add(f);
+                    // 递归
+                    dealFiles(all_next_files.ToArray(), Path.GetDirectoryName(out_dict_name));
+                }
+                else
+                {
+                    // deal simple file
+                    if(File.Exists(path)) dealOneFile(path,dict);
+                }
+                
+            }
+        }
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            var badstringFromDatabase = textBox1.Text;
+            var hopefullyRecovered = Encoding.GetEncoding("gb2312").GetBytes(badstringFromDatabase);
+            var oughtToBeJapanese = Encoding.GetEncoding(932).GetString(hopefullyRecovered);
+            textBox2.Text = oughtToBeJapanese;
+        }
+
+        private void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            try
+            {
+                string[] s = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+                if (s.Length > 0)
+                {
+                    dealFiles(s,null);
+                }
+            }
+            catch (Exception ex)
+            {
+                //print(ex.Message);
+            }
+        }
+
+        private void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.All;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+    }
+}
